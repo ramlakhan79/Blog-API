f4f3aea3ea1356f96a79d131a3bba9e1a431b4b0
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2026-09-10 00:42:33 +0530

    Update for Server file

--- server.js
+++ server.js
@@ -111,6 +111,10 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
